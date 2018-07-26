01cd2163e7f43c8d4b8a32ddf643aebc37c96f69
Nikolaus Kreiling <nakreiling@gmail.com>
2018-07-25 23:45:23 -0400

    Falldown and money system implemented
    Dummy falls down on user click and money is added whenever dummy falls down.
    Money is displayed on bottom left of screen

--- AdvGameDesign/Assets/Scripts/falldownonclick.cs
+++ AdvGameDesign/Assets/Scripts/falldownonclick.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class falldownonclick : MonoBehaviour {
 
     Animator anime;
 
+    public static int monies;
+    private int count;
+    public Text moniesText;
+
 	// Use this for initialization
 	void Start () {
         anime = gameObject.GetComponent<Animator>();
-        Debug.Log("bitchin'");
+        monies = 0;
+        moniesText.text = "Money: " + monies.ToString();
 	}
 	
 	// Update is called once per frame
@@ -17,10 +23,17 @@ public class falldownonclick : MonoBehaviour {
 		if (Input.GetButton("Fire1"))
         {
             anime.SetBool("FallDown", true);
+            if (count < 1)
+            {
+                monies = monies + 10;
+                count += 1;
+            }
+            moniesText.text = "Money: " + monies.ToString();
         }
         else
         {
             anime.SetBool("FallDown", false);
+            count = 0;
         }
 
 	}
