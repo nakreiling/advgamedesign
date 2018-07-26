6a41397cc6abf396823cd7f7d715664f0f5d2281
Nikolaus Kreiling <nakreiling@gmail.com>
2018-07-26 00:51:04 -0400

    Revert "Falldown and money system implemented"
    This reverts commit 01cd2163e7f43c8d4b8a32ddf643aebc37c96f69.

--- AdvGameDesign/Assets/Scripts/falldownonclick.cs
+++ AdvGameDesign/Assets/Scripts/falldownonclick.cs
@@ -1,21 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class falldownonclick : MonoBehaviour {
 
     Animator anime;
 
-    public static int monies;
-    private int count;
-    public Text moniesText;
-
 	// Use this for initialization
 	void Start () {
         anime = gameObject.GetComponent<Animator>();
-        monies = 0;
-        moniesText.text = "Money: " + monies.ToString();
+        Debug.Log("bitchin'");
 	}
 	
 	// Update is called once per frame
@@ -23,17 +17,10 @@ public class falldownonclick : MonoBehaviour {
 		if (Input.GetButton("Fire1"))
         {
             anime.SetBool("FallDown", true);
-            if (count < 1)
-            {
-                monies = monies + 10;
-                count += 1;
-            }
-            moniesText.text = "Money: " + monies.ToString();
         }
         else
         {
             anime.SetBool("FallDown", false);
-            count = 0;
         }
 
 	}
