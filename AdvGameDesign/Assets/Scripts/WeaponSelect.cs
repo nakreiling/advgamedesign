88c805b3f9c1e0ea30008832c2e4bc23c04bdee5
TFay <tfay1@uncc.edu>
2018-07-26 00:13:10 -0400

    Weapon Wheel n shit
    One instance of a weapon/hads can exist at one time. Gun animation works mainly. Gonna merge for demo now.

---
+++ AdvGameDesign/Assets/Scripts/WeaponSelect.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSelect : MonoBehaviour {
+
+
+        
+        public int selectedWeapon = 0;
+
+    // Use this for initialization
+    void Start()
+        {
+        SelectWeapon();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        selectedWeapon = 0;
+            int previousWeaponSelected = selectedWeapon;
+
+            if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+            {
+                if (selectedWeapon >= transform.childCount - 1)
+                    selectedWeapon = 0;
+                else
+                    selectedWeapon++;
+
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+            {
+                if (selectedWeapon <= transform.childCount - 1)
+                    selectedWeapon = 0;
+                else
+                    selectedWeapon++;
+
+            }
+            if (previousWeaponSelected != selectedWeapon)
+            {
+                SelectWeapon();
+            }
+
+        }
+        void SelectWeapon()
+        {
+            int i = 0;
+        foreach (Transform weapon in transform)
+        {
+           
+            if (i == selectedWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+
+            
+
+        }
+
+
+        }
+
+    }
+
+
+
+
