796c6a92d9afb242f6b85d2b2f60684b7a42290c
TFay <tfay1@uncc.edu>
2018-08-02 16:17:54 -0400

    This is all the stuff for Nick
    Here

---
+++ AdvGameDesign/Assets/Raycasting.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Raycasting : MonoBehaviour
+{
+
+    public Camera cam;
+    public LayerMask LayerMask;
+    public static bool isHit;
+
+    // Use this for initialization
+    void Start()
+    {
+        //find the game object that has the Player tag
+        //player = GameObject.FindWithTag("Player");
+        setHit(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("Mouse clicked");
+            Vector3 start = cam.transform.position;
+            Vector3 dir = cam.transform.forward * 100f;
+            RaycastHit hit;
+
+            if (Physics.Raycast(start, dir, out hit, Mathf.Infinity))
+            {
+                Debug.Log("Raycast initiated");
+                if (hit.collider.gameObject.tag == "YBot")
+                {
+                    Debug.Log("I shot someone.");
+                    setHit(true);
+                    Debug.Log(hit.collider.gameObject.name);
+                    Debug.DrawRay(start, dir, Color.red, 10.0f);
+                }
+                else
+                {
+                    Debug.Log("I missed");
+
+
+                    Debug.Log(hit.collider.gameObject.name);
+                    Debug.DrawRay(start, dir, Color.red, 10f);
+
+                }
+            }
+            Debug.DrawRay(start, dir, Color.red, 10f);
+            //EditorApplication.isPaused = true;
+        }
+    }
+
+    public static void setHit(bool var)
+    {
+        isHit = var;
+
+    }
+
+    public static bool getIsHit()
+    {
+        return isHit;
+    }
+}
